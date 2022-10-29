9b4b972e633a9f316dcd3258c52fd0e23e217346
RamilAssad <ramil_tolubaev@mail.ru>
2022-10-29 19:22:17 +0300

    first commit

--- ind.py
+++ ind.py
@@ -1,7 +1,11 @@
-def reverse():
-    x = int(input())
-    if x != 0:
-        reverse()
-    print(x)
+def alg(value, data):
+    if len(data) > 1:
+        return
+    for i in range(1, int(value**0.5) + 1):
+        res = value - i * i
+        if res > 0:
+            alg(res, data + [i])
+        elif res == 0:
+            print(' + '.join(f'{j} * {j}' for j in data + [i]), '=', sum(j * j for j in data + [i]))
 
-reverse()
+alg(int(input('введите число')), [])
